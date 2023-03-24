359161db879f13ee703beac404bd9d684c61a62f
vinhnguyen2k1 <thanhvinh951@gmail.com>
2023-03-24 20:05:04 +0700

    update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import httpError from 'http-errors';
 import { Connect } from './database/Connect.js';
-import userRouter from './Routers/userRouter.js'
-import updateRouter from './Routers/updateRouter.js'
+import userRouter from './Routers/userRouter.js';
+import updateRouter from './Routers/updateRouter.js';
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Connect to MongoDB
-Connect('mongodb+srv://jiro203201:12312311LHL@linh.evzju5i.mongodb.net/?retryWrites=true&w=majority');
+Connect('mongodb+srv://vinh:123123aA@cluster0.uojtt91.mongodb.net/?retryWrites=true&w=majority');
 
 //Use Router
 app.use('/api/users', userRouter);
@@ -20,25 +20,20 @@ app.use('/api/update', updateRouter);
 
 //404 handler and pass to errror handler
 app.use((request, response, next) => {
-    next(
-        httpError(
-            404, "Not Found"
-        ));
+	next(httpError(404, 'Not Found'));
 });
 
 //error handler
 app.use((error, request, response, next) => {
-    response.status(
-        error.status || 500
-    );
-    response.json({
-        error: {
-            status: error.status,
-            message: error.message
-        }
-    });
+	response.status(error.status || 500);
+	response.json({
+		error: {
+			status: error.status,
+			message: error.message,
+		},
+	});
 });
 
 app.listen(80, () => {
-    console.log('Server started on port 80...');
+	console.log('Server started on port 80...');
 });
